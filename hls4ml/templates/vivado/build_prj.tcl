4cc9bc63da09ee1f8a73ab84c4160214488b9b14
Javier M. Duarte <jduarte@ucsd.edu>
2021-12-07 21:29:46 -0800

    double limit

--- hls4ml/templates/vivado/build_prj.tcl
+++ hls4ml/templates/vivado/build_prj.tcl
@@ -64,7 +64,7 @@ if {$opt(reset)} {
 } else {
   open_solution "solution1"
 }
-catch {config_array_partition -maximum_size 4096}
+catch {config_array_partition -maximum_size 8192}
 config_compile -name_max_length 60
 set_part {xcku115-flvb2104-2-i}
 create_clock -period 5 -name default
